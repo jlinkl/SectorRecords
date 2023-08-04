af545eb4e99feb667a4ee779d93fe1cc63dab02d
James <james.m.linklater@gmail.com>
2023-08-04 16:48:59 +0100

    added classes for the tables

---
+++ models/lostsectors.py
@@ -0,0 +1,5 @@
+class LostSector:
+
+    def __init__(self, name, id=None):
+        self.name = name
+        self.id = id
---
+++ models/runs.py
@@ -0,0 +1,9 @@
+class Runs:
+
+    def __init__(self, link, runner, lostsector, season, id=None):
+        self.link = link
+        self.runner = runner
+        self.lostsector = lostsector
+        self.season = season
+        self.id = id
+        
