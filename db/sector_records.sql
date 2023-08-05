3b85c94366ca486ba5dcd5f4c5688e58578a2a11
James <james.m.linklater@gmail.com>
2023-08-05 18:41:32 +0100

    currently adding data into the tables manually.  with my current skills, and with how i understand the api, will require many fetches/calls to the api to get all the data i want, and isn't very efficient

--- db/sector_records.sql
+++ db/sector_records.sql
@@ -1,5 +1,5 @@
-DROP TABLE lost_sectors;
 DROP TABLE runs;
+DROP TABLE lost_sectors;
 DROP TABLE runners;
 DROP TABLE seasons;
 
@@ -26,3 +26,19 @@ CREATE TABLE runs (
     lost_sector_id INT NOT NULL REFERENCES lost_sectors(id) ON DELETE CASCADE,
     season_id INT NOT NULL REFERENCES seasons(id) ON DELETE CASCADE
 );
+
+INSERT INTO lost_sectors (name) VALUES ('Aphelions Rest'), ('Bay of Drowned Wishes'), 
+('Chamber of Starlight'), ('K1 Crew Quarters'), ('K1 Logistics'), ('K1 Communion'), ('K1 Revelation'),
+('Excavation Site'), ('Skydock IV'), ('The Quarry'), ('Scavengers Den'), ('Metamorphosis'), ('Sepulcher'),
+('Extraction'), ('Thrilladrome'), ('Hydropponics Delta'), ('Guilded Precept'), ('The Conflux'), ('Perdition'),
+('Bunker E15'), ('Concealed Void'), ('Exodus Garden 2A'), ('Veles Labyrinth');
+
+INSERT INTO runners (name) VALUES ('Skaryton'), ('Adlayy'), ('SheckelBoi'), ('Kev'), ('Taskeren'), 
+('Levie'), ('skrii'), ('Pegy16'), ('Tabbys'), ('xemo'), ('Tonal_'), ('Aoterra'), ('Suutek'), ('Barbuxx'),
+('Kapuhsky'), ('Clawtivity'), ('T J'), ('INathan162'), ('Theorii'), ('Supercool666');
+
+INSERT INTO seasons (name) VALUES ('Season of the Deep'), ('Season of Defiance'), ('Season of the Seraph'), ('Season of the Plunder'),
+('Season of the Haunted'), ('Season of the Risen'), ('Season of the Lost'), ('Season of the Splicer'), ('Season of the Chosen'),
+ ('Season of the Hunted');
+
+ INSERT INTO runs ()
