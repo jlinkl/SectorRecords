cc7db3f2e34a84dbfffe57dc354245bef21e38b5
James <james.m.linklater@gmail.com>
2023-08-07 17:54:00 +0100

    changed how i wanted runner to store data, and forgot to update in the model

--- models/runner.py
+++ models/runner.py
@@ -1,6 +1,5 @@
 class Runner:
 
-    def __init__(self, name, num_records, id=None):
+    def __init__(self, name, id=None):
         self.name = name
-        self.num_records = num_records
         self.id = id
