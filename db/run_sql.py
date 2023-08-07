def0f1cd9ee7c7b842c6fb810a16628b3cb99eca
James <james.m.linklater@gmail.com>
2023-08-07 18:49:53 +0100

    there was a syntax error in connecting to the db, testing pointed this out

--- db/run_sql.py
+++ db/run_sql.py
@@ -6,7 +6,7 @@ def run_sql(sql, values = None):
     results = []
 
     try:
-        conn=psycopg2.connect("name='sector_records")
+        conn=psycopg2.connect("dbname='sector_records'")
         cur = conn.cursor(cursor_factory=ext.DictCursor)
         cur.execute(sql, values)
         conn.commit()
