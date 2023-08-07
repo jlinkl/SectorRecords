3e81a2d7427bfac931138383d92dd8ba1000ab06
James <james.m.linklater@gmail.com>
2023-08-07 17:56:37 +0100

    updated comments

--- tests/runner_test.py
+++ tests/runner_test.py
@@ -5,10 +5,10 @@ class TestRunner(unittest.TestCase):
     def setUp(self):
         self.runner = Runner("Skaryton", 1)
 
-#testing to see if the lost sector class correctly holds its name
+#testing to see if the runner class correctly holds its name
     def test_class_name(self):
         self.assertEqual("Skaryton", self.runner.name)
 
-#testing to see if the lost sector class correctly holds its id
+#testing to see if the runner class correctly holds its id
     def test_class_id(self):
         self.assertEqual(1, self.runner.id)
